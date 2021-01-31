0431cd6d0d709d928e18b58815de606948c10c8f
Nicholas Silas <njsilas@gmail.com>
2021-01-31 04:06:52 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+function greet(time) {
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of greet() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
 /* Write your implementation of displayMessage() */
